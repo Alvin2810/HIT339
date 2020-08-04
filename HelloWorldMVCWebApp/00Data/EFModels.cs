fbbe85542538b00a0236385c68104e0b03134acd
Alvin Mammoottil <monsialvin@gmail.com>
2020-08-04 12:21:47 +0930

    Creating Movie web application

---
+++ HelloWorldMVCWebApp/00Data/EFModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVCWebApp._00Data
+{
+    public class EFModels
+    {
+        public int MovieId { get; set;}
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string EmailAddress { get; set; }
+        public string Language { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ HelloWorldMVCWebApp/00Data/MovieDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorldMVCWebApp._00Data
+{
+    public class MovieDBContext: DbContext
+    {
+        public MovieDBContext(DbContextOptions<MovieDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
